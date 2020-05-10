b72da2524bbcb01bfcf969ff55398f68f5d38cdf
Rafael Paixão <rafaelsspaixao@gmail.com>
2020-05-10 17:29:30 -0300

    new vue project with jointjs's erd demo

---
+++ src/vue-erd/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="paper"></div>
+</template>
+<script>
+import 'jointjs/dist/joint.min.css'
+import * as joint from 'jointjs'
+import erdDemo from './erd-demo'
+
+export default {
+  mounted () {
+    erdDemo(joint)
+  }
+}
+</script>
+<style lang="scss">
+#paper {
+    width: inherit;
+    display: block;
+    border: 0;
+    margin-bottom: 3px;
+    text-align: center;
+    background: #464a65;
+    &>svg {
+        overflow: visible;
+    }
+    .connection,
+    .connection-wrap {
+        stroke: #ccc0ef;
+    }
+}
+</style>
