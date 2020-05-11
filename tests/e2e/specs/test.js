0d29734f44f8da2d891505e8e011d3a4b4461ad7
Rafael Paixão <rafaelsspaixao@gmail.com>
2020-05-11 01:01:35 -0300

    add e2e test that verify if the diagram ans renderd and drags an entity

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,27 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+describe('Vue-ERD Test', () => {
+  it('Verify that the diagram was rendered', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.get('#paper')
+      .should('be.visible')
+      .should('have.class', 'joint-paper')
+  })
+
+  it('Drag an entity', () => {
+    const FINAL_X = 100
+    const FINAL_Y = 500
+    cy.get('#paper')
+      .find('[data-type="erd.Entity"]')
+      .should('be.visible')
+      .first()
+      .trigger('mousedown', 'topLeft', { which: 1 })
+      .trigger('mousemove', { which: 1, clientX: FINAL_X, clientY: FINAL_Y })
+      .trigger('mouseup')
+      .then($target => {
+        const { x, y } = $target[0].getBoundingClientRect()
+        expect(x).to.equal(FINAL_X)
+        expect(y).to.equal(FINAL_Y)
+      })
   })
 })
